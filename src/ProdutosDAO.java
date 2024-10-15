d702bad8dc4930ebbd075e743bee921ad1b0acc2
Mikallina <mi_borges@msn.coma>
2024-10-15 13:55:27 -0300

    Criando listarProdutosVendidos

--- src/ProdutosDAO.java
+++ src/ProdutosDAO.java
@@ -47,6 +47,27 @@ public class ProdutosDAO {
 	        return false;
 	    }
 	}
+   public ArrayList<ProdutosDTO> listarProdutosVendidos(){
+   ArrayList<ProdutosDTO> listagem = new ArrayList<>();
+   String sql = "SELECT * FROM produtos WHERE status = 'Vendido'";
+        try (Connection conn = conecta.connectDB(); 
+             PreparedStatement prep = conn.prepareStatement(sql);
+             ResultSet resultSet = prep.executeQuery()) {
+
+            while (resultSet.next()) {
+                ProdutosDTO produto = new ProdutosDTO();
+                produto.setId(resultSet.getInt("id"));
+                produto.setNome(resultSet.getString("nome"));
+                produto.setValor(resultSet.getInt("valor"));
+                produto.setStatus(resultSet.getString("status"));
+                listagem.add(produto);
+            }
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, "Erro ao listar produtos Vendidos: " + e.getMessage());
+        }
+        return listagem;
+      }
+
 
     public ArrayList<ProdutosDTO> listarProdutos() {
         ArrayList<ProdutosDTO> listagem = new ArrayList<>();
